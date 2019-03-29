1e7a0b98c4dbadbcf9b442f6d29286ade1a3cc25
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-03-29 16:17:36 -0500

    Update deploy.ps1

--- Public/lib/Storage/deploy.ps1
+++ Public/lib/Storage/deploy.ps1
@@ -1,5 +1,5 @@
 # Deploy the Resource Group & Storage Account
-$deploy = New-AzureRmDeployment -Location eastus -TemplateParameterFile .\deploy_Customer_parameters.json -TemplateFile .\deploy_Customer.json -Verbose
+$deploy = New-AzureRmDeployment -Name Test1 -Location eastus -TemplateParameterFile .\deploy_Customer_parameters.json -TemplateFile .\deploy_Customer.json -Verbose
 
 # Retrieve the Storage Account details
 $resourceGroup = $deploy.Outputs.Values.value[0]
@@ -7,8 +7,8 @@ $storageAccountName = $deploy.Outputs.Values.value[1]
 $storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroup -AccountName $storageAccountName).Value[0]
 
 # Deploy the Azure File Share
-$AzureFileShareName = @('emsADI','emsUser','emsCollections')
+$AzureFileShareName = @('emsadi','emsuser','emscollections')
 $storageContext = New-AzureStorageContext $storageAccountName $storageAccountKey
 foreach($azrfile in $AzureFileShareName){
-$share = New-AzureStorageShare $AzureFileShareName -Context $storageContext
+$share = New-AzureStorageShare $azrfile -Context $storageContext
 }
