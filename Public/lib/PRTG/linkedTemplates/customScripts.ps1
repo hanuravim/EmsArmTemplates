ceaca9140728948be50ae25dbc777ba7361e81d6
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-25 08:01:12 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -1,13 +1,11 @@
 Param (
-  [Parameter()]
-  [String]$PRTG_SetupLocation,
-  [String]$PRTG_DownloadLocation
-  
+    [Parameter()]
+    [String]$PRTG_SetupLocation
 )
 
 # Download path for PRTG setup
 $File = $($env:TEMP) + '\PRTG.EXE'
 # Start Download
-Invoke-WebRequest -Uri $Uri -OutFile $File
+Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
 # Install
 Start-Process -Wait -FilePath $File -ArgumentList "/S /v /qn" -passthru
