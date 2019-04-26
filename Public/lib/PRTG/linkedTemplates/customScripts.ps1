7a0027996b8a03679689428d144da30ea7425660
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-26 10:37:12 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -3,8 +3,8 @@ Param (
     [String]$PRTG_SetupLocation
 )
 # Download path for PRTG setup
-$File = 'D:\' + '\PRTG.EXE'
+$File = $($env:TEMP) + '\PRTG.EXE'
 # Start Download
 Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
 # Install
-Start-Process -Wait -FilePath $File -ArgumentList "/S /v /qn" -passthru
+Start-Process -Wait -FilePath $File -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES"
