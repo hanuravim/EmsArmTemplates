a220b8119f2ba473bd737fff1ad43b4588b0cfb6
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-26 11:04:42 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -7,4 +7,4 @@ $File = $($env:TEMP) + '\PRTG.EXE'
 # Start Download
 Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
 # Install
-Start-Process -Wait -FilePath $File -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES"
+Start-Process -Wait -FilePath $File -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES" -PassThru
