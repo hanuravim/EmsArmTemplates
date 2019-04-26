d117ab9a4d4b880443f0eaa3bd079e2349ed247a
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-26 05:47:20 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -8,4 +8,7 @@ $File = 'D:\' + '\PRTG.EXE'
 # Start Download
 Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
 # Install
-Start-Process -Wait -FilePath $File -ArgumentList "/S /v /qn" -passthru
+$myshell = New-Object -com "Wscript.Shell"
+$myshell.sendkeys("{ENTER}")
+
+#Start-Process -Wait -FilePath $File -ArgumentList "/S /v /qn" -passthru
