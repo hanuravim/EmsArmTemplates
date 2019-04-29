189f0c263f9706bd88f097e92640266102106db0
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-29 05:10:49 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -1,12 +1,13 @@
 Param (
     [Parameter()]
-    [String]$PRTG_SetupLocation
+    [String]$PRTG_SetupLocation = "https://dremsblobstore.blob.core.windows.net/scripts/PRTG_Remote_Probe_Installer_for_adiMonitoring.us.ae.ge.com_with_key_{A30FFAC8}.exe"
 )
 # Download path for PRTG setup
 $File = $($env:TEMP) + '\PRTG.EXE'
 # Start Download
-Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
+#Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
+
 # Install
-$command = “cmd.exe /c $file /silent /v`”/qn”
+$command = “cmd.exe /c $file /silent /v /qn"
 $process = [WMICLASS]“\\$env:COMPUTERNAME\ROOT\CIMV2:win32_process“
 $process.Create($command)
