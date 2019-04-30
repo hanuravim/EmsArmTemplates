93b1b45c4c0853b147bd7e3fd81c51ab75c50add
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-30 09:24:01 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -4,5 +4,9 @@ Param (
     [String]$VMName
 
 )
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Az -Confirm:$False
+Import-Module Az
 Restart-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName
 Remove-AzVMExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Name 'customScripts' -Force
