1022c069120949cac2ceb682d7c7471fc3af3f12
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-04-30 23:44:33 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -6,7 +6,9 @@ Param (
 )
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
 Install-Module AzureRm -Confirm:$False
+Import-Module Azure
 Import-Module AzureRm
 Restart-Computer -Force
 Remove-AzureRmVMExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Name 'customScripts' -Force
