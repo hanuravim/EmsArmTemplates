3d03aca3a9201bf3c18faefc6e5e0ff523e9e5f7
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-05-19 22:55:22 -0500

    Delete customScripts.ps1

--- Public/lib/JumpBox/customScripts.ps1
+++
@@ -1,9 +0,0 @@
-Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
-Get-WindowsFeature -Name Web-* | Install-WindowsFeature
-Install-WindowsFeature –Name 'GPMC'
-Get-WindowsFeature -Name RSAT-* | Install-WindowsFeature
-Get-WindowsFeature -Name Hyper-V-* | Install-WindowsFeature
-Get-WindowsFeature -Name Telnet-*  | Install-WindowsFeature
-Get-WindowsFeature -Name Windows-Defender-*  | Install-WindowsFeature
-Install-WindowsFeature -Name UpdateServices, UpdateServices-RSAT, UpdateServices-API, UpdateServices-UI
-Restart-Computer -Force
