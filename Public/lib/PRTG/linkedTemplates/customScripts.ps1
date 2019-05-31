d1ab6fa6925b0529aac4938810ef9b0d45e019d5
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-05-31 09:43:15 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -1,11 +1,11 @@
 Param (
     [Parameter()]
-    [String]$PRTG_SetupLocation = "https://dremsblobstore.blob.core.windows.net/scripts/PRTG_Remote_Probe_Installer_for_adiMonitoring.us.ae.ge.com_with_key_{A30FFAC8}.exe"
+    [String]$PRTG_SetupLocation
 )
 # Download path for PRTG setup
 $File = $($env:TEMP) + '\PRTG.EXE'
 # Start Download
 Invoke-WebRequest -Uri $PRTG_SetupLocation -OutFile $File
 # Install
-Start-Process -Wait -FilePath $File -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES" -PassThru
-Restart-Computer -Force 
+Set-Location -Path $env:TEMP
+.\PRTG.EXE /VERYSILENT /SUPPRESSMSGBOXES
