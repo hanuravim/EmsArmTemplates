1c0ec2dfe930844ea986094adbb7615aaa1a215c
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-05-30 22:21:46 -0500

    Update customScripts.ps1

--- Public/lib/PRTG/linkedTemplates/customScripts.ps1
+++ Public/lib/PRTG/linkedTemplates/customScripts.ps1
@@ -1,6 +1,6 @@
 Param (
     [Parameter()]
-    [String]$PRTG_SetupLocation
+    [String]$PRTG_SetupLocation = "https://dremsblobstore.blob.core.windows.net/scripts/PRTG_Remote_Probe_Installer_for_adiMonitoring.us.ae.ge.com_with_key_{A30FFAC8}.exe"
 )
 # Download path for PRTG setup
 $File = $($env:TEMP) + '\PRTG.EXE'
