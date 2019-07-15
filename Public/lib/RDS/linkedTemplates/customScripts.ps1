dc8aff50c67baf762ca70a66b30fa69948180066
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-07-14 19:58:24 -0500

    Update customScripts.ps1

--- Public/lib/RDS/linkedTemplates/customScripts.ps1
+++ Public/lib/RDS/linkedTemplates/customScripts.ps1
@@ -1,4 +1,1 @@
-#REMOTE DESKTOP GATEWAY
-Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
-$fqdn = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
-New-RDSessionDeployment –ConnectionBroker $fqdn –WebAccessServer $fqdn –SessionHost $fqdn
+Add-WindowsFeature –Name RDS-RD-Server –IncludeAllSubFeature -Restart
