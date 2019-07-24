9dafd9e73903744a1ca5b00ce37948f2ef315d3c
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-07-24 12:20:51 -0500

    Update customScripts.ps1

--- Public/lib/RDS/linkedTemplates/customScripts.ps1
+++ Public/lib/RDS/linkedTemplates/customScripts.ps1
@@ -1,35 +1,4 @@
-param (
-    [Parameter(Mandatory=$False, HelpMessage="FQDN of RD Web Access\RD Connection Broker and RD Session host roles")]
-    [String]
-    $serverName='EDSRDRDS01.efoqa.local',
-
-    [Parameter(Mandatory=$False, HelpMessage="Session Collection Name")]
-    [String]
-    $SessionCollectionName='rdssc',
-	
-	[Parameter(Mandatory=$False, HelpMessage="Session Discription")]
-    [String]
-    $CollectionDiscription='rdscd',
-
-    [Parameter(Mandatory=$False, HelpMessage="Domain name ")]
-    [String]
-    $domain='efoqa.local'
-)
-
-# Import the RemoteDesktop module
-Import-Module RemoteDesktop
-
-# Create a new RDS deployment
-New-RDSessionDeployment -ConnectionBroker $serverName `
-   -WebAccessServer $serverName `
-   -SessionHost $serverName 
-Write-Verbose "Created new RDS deployment on: $serverName"
-
-
-# Create a new pooled managed desktop collection
-New-RDSessionCollection -CollectionName $SessionCollectionName `
-   -SessionHost $serverName `
-   -CollectionDescription $CollectionDiscription `
-   -ConnectionBroker $serverName `
-  
-   
+#REMOTE DESKTOP GATEWAY
+Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
+$fqdn = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+New-RDSessionDeployment –ConnectionBroker $fqdn –WebAccessServer $fqdn –SessionHost $fqdn
