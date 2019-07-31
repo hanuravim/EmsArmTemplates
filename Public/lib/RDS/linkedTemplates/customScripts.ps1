574abd37e90f0153d4dc9b5800489ef7d3938d59
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-07-31 18:02:19 -0500

    Update customScripts.ps1

--- Public/lib/RDS/linkedTemplates/customScripts.ps1
+++ Public/lib/RDS/linkedTemplates/customScripts.ps1
@@ -1,4 +1,14 @@
-#REMOTE DESKTOP GATEWAY
-Install-WindowsFeature -Name 'RDS-Gateway' -IncludeAllSubFeature
-$fqdn = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
-New-RDSessionDeployment –ConnectionBroker $fqdn –WebAccessServer $fqdn –SessionHost $fqdn
+Install-WindowsFeature @(
+    'RDS-Connection-Broker'
+    'RDS-Licensing'
+    'RDS-RD-Server'
+    'RDS-Web-Access' 
+) -IncludeAllSubFeature -IncludeManagementTools -Restart
+
+# Must be domain joined computer, logged in with domain account
+Import-Module RemoteDesktop
+$FQDN = "$($env:COMPUTERNAME).$($env:USERDNSDOMAIN)"
+New-RDSessionDeployment -ConnectionBroker $FQDN -WebAccessServer $FQDN -SessionHost $FQDN # install the three compulsory RDS components
+
+# associate the new license server with your existing Connection Broker
+Set-RDLicenseConfiguration -LicenseServer $FQDN -Mode PerUser -ConnectionBroker $FQDN -Force 
